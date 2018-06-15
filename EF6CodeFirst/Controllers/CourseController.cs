45ab74fd89c02faa9a5194ee0a493ee32de6f164
Ryan <claryan92@gmail.com>
2018-06-15 13:31:33 -0700

    Updating related data by updating foreign key fields and navigation properties. This also required adding and removing entities from navigation properties.

--- EF6CodeFirst/Controllers/CourseController.cs
+++ EF6CodeFirst/Controllers/CourseController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -40,7 +41,7 @@ namespace EF6CodeFirst.Controllers
         // GET: Course/Create
         public ActionResult Create()
         {
-            ViewBag.DepartmentID = new SelectList(db.Departments, "DepartmentID", "Name");
+			PopulateDepartmentsDropDownList();
             return View();
         }
 
@@ -51,14 +52,20 @@ namespace EF6CodeFirst.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "CourseID,Title,Credits,DepartmentID")] Course course)
         {
-            if (ModelState.IsValid)
-            {
-                db.Courses.Add(course);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            ViewBag.DepartmentID = new SelectList(db.Departments, "DepartmentID", "Name", course.DepartmentID);
+			try
+			{
+				if (ModelState.IsValid)
+				{
+					db.Courses.Add(course);
+					db.SaveChanges();
+					return RedirectToAction("Index");
+				}
+			}
+			catch (RetryLimitExceededException)
+			{
+				ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
+			}
+			PopulateDepartmentsDropDownList(course.DepartmentID);
             return View(course);
         }
 
@@ -74,29 +81,47 @@ namespace EF6CodeFirst.Controllers
             {
                 return HttpNotFound();
             }
-            ViewBag.DepartmentID = new SelectList(db.Departments, "DepartmentID", "Name", course.DepartmentID);
+			PopulateDepartmentsDropDownList(course.DepartmentID);
             return View(course);
         }
 
         // POST: Course/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
+        [HttpPost, ActionName("Edit")]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "CourseID,Title,Credits,DepartmentID")] Course course)
+        public ActionResult EditPost(int? id)
         {
-            if (ModelState.IsValid)
-            {
-                db.Entry(course).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.DepartmentID = new SelectList(db.Departments, "DepartmentID", "Name", course.DepartmentID);
-            return View(course);
-        }
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			var courseToUpdate = db.Courses.Find(id);
+			if (TryUpdateModel(courseToUpdate, "",
+					new string[] { "Title", "Credits", "DepartmentID" }))
+			{
+				try
+				{
+					db.SaveChanges();
+					return RedirectToAction("Index");
+				}
+				catch (RetryLimitExceededException)
+				{
+					ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists contact your system administrator.");
+				}
+			}
+			PopulateDepartmentsDropDownList(courseToUpdate.DepartmentID);
+			return View(courseToUpdate);
+		}
 
-        // GET: Course/Delete/5
-        public ActionResult Delete(int? id)
+		private void PopulateDepartmentsDropDownList(object selectedDepartment = null)
+		{
+			var departmentsQuery = from d in db.Departments
+								   orderby d.Name
+								   select d;
+			ViewBag.DepartmentID = new SelectList(departmentsQuery, "DepartmentID", "Name", selectedDepartment);
+		}
+
+		// GET: Course/Delete/5
+		public ActionResult Delete(int? id)
         {
             if (id == null)
             {
